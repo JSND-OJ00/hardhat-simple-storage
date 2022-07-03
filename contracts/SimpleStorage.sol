f94d6db73295845c7b272ce7529034007a2c6e91
JSND-OJ00 <jsnd.oj@gmail.com>
2022-07-03 21:27:13 +0900

    got it

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -7,28 +7,31 @@ pragma solidity 0.8.8;
 // pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-  uint256 favoriteNumber;
-
-  struct People {
     uint256 favoriteNumber;
-    string name;
-  }
 
-  // uint256[] public anArray;
-  People[] public people;
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
 
-  mapping(string => uint256) public nameToFavoriteNumber;
+    // uint256[] public anArray;
+    // People 타입을 요소로 갖는 people이라는 이름의 배열
+    People[] public people;
 
-  function store(uint256 _favoriteNumber) public {
-    favoriteNumber = _favoriteNumber;
-  }
+    // string을 key로, uint256을 value로 짝지어 줍니다.
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-  function retrieve() public view returns (uint256) {
-    return favoriteNumber;
-  }
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber; // 다르다는 것을 명시하기 위해 _를 붙여줍니다.
+    }
 
-  function addPerson(string memory _name, uint256 _favoriteNumber) public {
-    people.push(People(_favoriteNumber, _name));
-    nameToFavoriteNumber[_name] = _favoriteNumber;
-  }
+    //  view 함수는 단순히 조회만 가능합니다.
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name)); // people 배열에 새로운 People을 추가합니다.
+        nameToFavoriteNumber[_name] = _favoriteNumber; // key와 value 짝을 만듭니다.
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,51 +1,55 @@
 // imports
-const { ethers, run, network } = require("hardhat")
+const { ethers, run, network } = require("hardhat");
 
 // async main
 async function main() {
-  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
-  console.log("Deploying contract...")
-  const simpleStorage = await SimpleStorageFactory.deploy()
-  await simpleStorage.deployed()
-  console.log(`Deployed contract to: ${simpleStorage.address}`)
+  // 컨트랙트를 불러옵니다.
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract...");
+  // 불러온 컨트랙트를 배포합니다.
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  // 배포된 컨트랙트의 주소를 획득합니다.
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
   // what happens when we deploy to our hardhat network?
+  // chainId: An optional number, used to validate the network Hardhat connects to. If not present, this validation is omitted.
   if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
-    console.log("Waiting for block confirmations...")
-    await simpleStorage.deployTransaction.wait(6)
-    await verify(simpleStorage.address, [])
+    console.log("Waiting for block confirmations...");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
   }
 
-  const currentValue = await simpleStorage.retrieve()
-  console.log(`Current Value is: ${currentValue}`)
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
 
   // Update the current value
-  const transactionResponse = await simpleStorage.store(7)
-  await transactionResponse.wait(1)
-  const updatedValue = await simpleStorage.retrieve()
-  console.log(`Updated Value is: ${updatedValue}`)
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Value is: ${updatedValue}`);
 }
 
 // async function verify(contractAddress, args) {
 const verify = async (contractAddress, args) => {
-  console.log("Verifying contract...")
+  console.log("Verifying contract...");
   try {
     await run("verify:verify", {
       address: contractAddress,
       constructorArguments: args,
-    })
+    });
   } catch (e) {
     if (e.message.toLowerCase().includes("already verified")) {
-      console.log("Already Verified!")
+      console.log("Already Verified!");
     } else {
-      console.log(e)
+      console.log(e);
     }
   }
-}
+};
 
 // main
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    console.error(error);
+    process.exit(1);
+  });
